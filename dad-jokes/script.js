0c9746c7f7d2ac1f688ce0e9c76e093ebaabe2e1
Torrence Cole <trcole@gmail.com>
2021-01-06 09:52:15 -0500

    Dad jokes project finsihed

---
+++ dad-jokes/script.js
@@ -0,0 +1,36 @@
+const jokeEL = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await res.json();
+
+  jokeEL.innerHTML = data.joke;
+}
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEL.innerHTML = data.joke;
+//     });
+// }
