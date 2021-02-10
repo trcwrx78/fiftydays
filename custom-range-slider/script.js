39c99ae325cf768c5d161ddbd27dc97d603d099a
Torrence Cole <trcole@gmail.com>
2021-02-10 10:02:34 -0500

    netflix mobile nav project

--- custom-range-slider/script.js
+++ custom-range-slider/script.js
@@ -4,15 +4,19 @@ range.addEventListener('input', (e) => {
   const value = +e.target.value;
   const label = e.target.nextElementSibling;
 
+  // Gets the css value for the width of the elements
   const range_width = getComputedStyle(e.target).getPropertyValue('width');
   const label_width = getComputedStyle(label).getPropertyValue('width');
 
+  // Converts to number and cuts off the px
   const num_width = +range_width.substring(0, range_width.length - 2);
   const num_label_width = +label_width.substring(0, label_width.length - 2);
 
+  // Gets max and min from the HTML element
   const max = +e.target.max;
   const min = +e.target.min;
 
+  // Calculates the left css property to slide the number box
   const left =
     value * (num_width / max) -
     num_label_width / 2 +
